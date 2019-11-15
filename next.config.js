86c9f1da11f41597a4306394b387b8f998aedef0
Max Sosnov <crazymax101@gmail.com>
2019-11-15 23:54:26 +0700

    Обновляет фавикон

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 // next.config.js
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const withOffline = require('next-offline')
 
 module.exports = withPlugins([
   [optimizedImages, {
       optimizeImages: false
-  }]
-
+  }],
+  withOffline()
 ]);
